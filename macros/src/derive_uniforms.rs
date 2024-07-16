b12fa4f65f5adafb227c34e21f6f5d31992d74b1
hevezolly <sevafinogenov@gmail.com>
2024-07-16 12:48:37 +0300

    cleanup

--- macros/src/derive_uniforms.rs
+++ macros/src/derive_uniforms.rs
@@ -1,13 +1,16 @@
-use std::{env::var, fs, path::PathBuf};
+use std::{fs, path::PathBuf};
 
-use glsl::{parser::Parse, syntax::{ShaderStage, SingleDeclaration, StorageQualifier, TypeQualifierSpec, TypeSpecifierNonArray}};
+use glsl::{parser::Parse, syntax::{SingleDeclaration, StorageQualifier, TypeQualifierSpec, TypeSpecifierNonArray}};
 use proc_macro::TokenStream;
-use proc_macro2::TokenStream as TokenStream2;
 use quote::ToTokens;
 use syn::{parse::Parser, parse_macro_input, punctuated::Punctuated, Field, ItemStruct, LitStr, Token, Type};
 
 
-fn glsl_type_to_rust_types(glsl_type: &TypeSpecifierNonArray, shader_name: &str) -> proc_macro2::TokenStream {
+const OPTIONAL_PATTERN: &'static str = "//!OPTIONAL";
+const FILE_PATHS_ATTRIBUTE: &'static str = "for_shaders";
+const NAME_ATTRIBUTE: &'static str = "name";
+
+fn glsl_type_to_rust_type(glsl_type: &TypeSpecifierNonArray, shader_name: &str) -> proc_macro2::TokenStream {
     match glsl_type {
         TypeSpecifierNonArray::Bool => quote!(bool),
         TypeSpecifierNonArray::Int => quote!(i32),
@@ -38,15 +41,15 @@ fn glsl_type_to_rust_types(glsl_type: &TypeSpecifierNonArray, shader_name: &str)
     }
 }
 
-struct UniformDef {
+struct UniformDefinition {
     name: String,
     rust_type: proc_macro2::TokenStream,
     optional: bool,
 }
 
-impl UniformDef {
-    fn try_from_line(line: &str, shader_name: &str) -> Option<UniformDef> {
-        let optional = line.contains("//!OPTIONAL");
+impl UniformDefinition {
+    fn try_from_line(line: &str, shader_name: &str) -> Option<UniformDefinition> {
+        let optional = line.contains(OPTIONAL_PATTERN);
         let declaration = SingleDeclaration::parse(line).ok()?;
         let has_uniform = declaration.ty.qualifier?.qualifiers.0.iter()
             .any(|q| match q {
@@ -64,20 +67,12 @@ impl UniformDef {
             panic!("\"{}\" shader error: array uniforms not supported", shader_name);
         };
 
-        let rust_type = glsl_type_to_rust_types(&uni_type_decl.ty, shader_name);
+        let rust_type = glsl_type_to_rust_type(&uni_type_decl.ty, shader_name);
 
 
-        Some(UniformDef { name, rust_type, optional })
+        Some(UniformDefinition { name, rust_type, optional })
     }
 }
-
-fn field_attrib_name(field: &Field) -> Option<String> {
-    let attrib = field.attrs.iter().find(|a| a.path().is_ident("name"))?;
-    let lit: LitStr = attrib.parse_args().expect("attribute name should contain string literal");
-
-    Some(lit.value())
-}
-
 
 pub fn derive_uniforms_internal(item: TokenStream) -> TokenStream {
     let parsed_item = parse_macro_input!(item as ItemStruct);
@@ -87,12 +82,55 @@ pub fn derive_uniforms_internal(item: TokenStream) -> TokenStream {
     let (field_names, field_types): (Vec<_>, Vec<_>) = 
         parsed_item.fields.iter().map(|f| (&f.ident, &f.ty)).unzip();
 
-    let uniform_names: Vec<_> = parsed_item.fields.iter()
-        .map(|f| field_attrib_name(f).unwrap_or_else(|| f.ident.to_token_stream().to_string()) ).collect();
+    let uniform_names: Vec<_> = parsed_item.fields
+        .iter()
+        .map(field_name)
+        .collect();
 
+    let uniform_types = get_uniform_types_from_file(&parsed_item, &uniform_names);
+
+
+    let result = quote!(
+        impl core::shaders::uniforms::Uniforms for #self_type {
+            fn apply_uniforms(&self, names_mapping: &std::collections::HashMap<String, i32>) {
+                #(core::shaders::uniforms::UniformCompatableType::apply_by_name(&self.#field_names, #uniform_names, names_mapping);)*
+            }
+            
+            fn defenition() -> Vec<String> {
+                #(let compatable = core::shaders::uniforms::check_uniform_compatable::<#field_types, #uniform_types>();)*
+                // #(let compatable = <#fied_types as core::shaders::uniforms::UniformCompatableType<Target = #uniform_types>>::IS_COMPATABLE;)*
+                // #(let alias = <#fied_types as core::OpenglAlias<#uniform_types>>::IS_ALIAS;)*
+                vec![#(#uniform_names.to_owned(),)*]
+            }
+        }
+    );
+
+    result.into()
+}
+
+fn field_name(field: &Field) -> String {
+    let attribute = field.attrs.iter().find(|a| a.path().is_ident(NAME_ATTRIBUTE));
+    
+    if let Some(attribute) = attribute {
+        attribute
+            .parse_args::<LitStr>()
+            .expect("attribute name should contain string literal")
+            .value()
+    }
+    else {
+        field.ident
+            .to_token_stream()
+            .to_string()
+    }
+}
+
+fn get_uniform_types_from_file(
+    parsed_item: &ItemStruct,
+    uniform_names: &Vec<String>
+) -> Vec<Type> {
     let mut uniform_types = Vec::new();
 
-    if let Some(uniforms) = get_file_uniforms(&parsed_item) {
+    if let Some(uniforms) = get_uniforms_from_file(&parsed_item) {
         let mut used_uniforms = Vec::with_capacity(uniforms.len());
         for field_name in uniform_names.iter() {
             if let Some(uniform) = uniforms.iter().find(|u| &u.name == field_name) {
@@ -121,39 +159,23 @@ pub fn derive_uniforms_internal(item: TokenStream) -> TokenStream {
         }
     }
 
-
-    let result = quote!(
-        impl core::shaders::uniforms::Uniforms for #self_type {
-            fn apply_uniforms(&self, names_mapping: &std::collections::HashMap<String, i32>) {
-                #(core::shaders::uniforms::UniformCompatableType::apply_by_name(&self.#field_names, #uniform_names, names_mapping);)*
-            }
-            
-            fn defenition() -> Vec<String> {
-                #(let compatable = core::shaders::uniforms::check_uniform_compatable::<#field_types, #uniform_types>();)*
-                // #(let compatable = <#fied_types as core::shaders::uniforms::UniformCompatableType<Target = #uniform_types>>::IS_COMPATABLE;)*
-                // #(let alias = <#fied_types as core::OpenglAlias<#uniform_types>>::IS_ALIAS;)*
-                vec![#(#uniform_names.to_owned(),)*]
-            }
-        }
-    );
-
-    result.into()
+    uniform_types
 }
 
-fn get_file_uniforms(parsed_item: &ItemStruct) -> Option<Vec<UniformDef>> {
-    let from = parsed_item.attrs.iter().find(|a| a.path().is_ident("for_shaders"));
-
-    let mut result = Vec::<UniformDef>::new();
-
-    let mut has_froms = false;
+fn get_uniforms_from_file(parsed_item: &ItemStruct) -> Option<Vec<UniformDefinition>> {
+    let file_paths_source = parsed_item.attrs.iter().find(|a| a.path().is_ident(FILE_PATHS_ATTRIBUTE));
+    
     let parser = Punctuated::<LitStr, Token![,]>::parse_separated_nonempty;
 
-    if let Some(f) = from {
-        has_froms = true;
-        let paths = f.parse_args_with(parser).unwrap();
-        for lit in paths {
-            let path_str = lit.value();
-            let path = PathBuf::from(&path_str).canonicalize().expect(&format!("path {} does not exist", &path_str));
+    if let Some(p) = file_paths_source {
+        let mut result = Vec::<UniformDefinition>::new();
+
+        let paths = p.parse_args_with(parser).unwrap();
+        for literal in paths {
+            let path_str = literal.value();
+            let path = PathBuf::from(&path_str)
+                .canonicalize()
+                .expect(&format!("path {} does not exist", &path_str));
             
             if !path.is_file() {
                 panic!("path {} should be a file", path_str);
@@ -166,8 +188,12 @@ fn get_file_uniforms(parsed_item: &ItemStruct) -> Option<Vec<UniformDef>> {
     
             let source = fs::read_to_string(path).unwrap();
     
-            for uniform in source.lines().filter_map(|l| UniformDef::try_from_line(l, name)) {
-                if let Some(UniformDef { name, rust_type, optional: _ }) = result.iter().find(|f| f.name == uniform.name) {
+            for uniform in source.lines()
+                .filter_map(|l| UniformDefinition::try_from_line(l, name)) 
+            {
+                if let Some(UniformDefinition { name, rust_type, optional: _ }) = result.iter()
+                    .find(|f| f.name == uniform.name) 
+                {
                     if uniform.rust_type.to_string() != rust_type.to_string() {
                         panic!("uniform '{}' is declared multiple times with different types", name);
                     }
@@ -176,13 +202,9 @@ fn get_file_uniforms(parsed_item: &ItemStruct) -> Option<Vec<UniformDef>> {
                 result.push(uniform);
             }
         }
-    };
-
-    if has_froms {
         Some(result)
     }
     else {
         None
     }
-
 }
